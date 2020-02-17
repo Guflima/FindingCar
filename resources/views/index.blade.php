3c5eb3d9271e083cd7a674ae3eca2ad79edb1ccf
martins-2002 <60940110+martins-2002@users.noreply.github.com>
2020-02-17 14:09:35 -0300

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
             <div class="form-row">
               <div class="col-12 col-md-9 mb-2 mb-md-0">
                 <input type="email" class="form-control form-control-lg" placeholder="Encontre seu veículo">
-              </div>
+                </div>
               <div class="col-12 col-md-3">
                 <button type="submit" class="btn btn-block btn-lg btn-danger">Vamos lá!</button>
               </div>
